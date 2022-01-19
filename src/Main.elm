64cf42275fb76a1fcd5861f4ae8786657d871137
Johannes Kvamme <johannes.kvamme@bekk.no>
2022-01-19 15:35:56 +0100

    Add Vite to work for M1-chips as well

--- src/Main.elm
+++ src/Main.elm
@@ -5,4 +5,4 @@ import Html exposing (..)
 
 
 main =
-    "Hello, world!"
+    text "
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-require('./main.css');
-var Elm = require('./Main.elm');
+import './main.css';
+import { Elm } from './Main.elm';
 
+
+const node = document.querySelector("#app div")
 // Bug i elm-webpack-loader gjør at vi får Elm.Elm her; https://github.com/elm-community/elm-webpack-loader/issues/149
-Elm.Elm.Main.init({ node: document.getElementById('root') });
+Elm.Main.init({ node: node });
