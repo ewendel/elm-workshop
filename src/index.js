06e6a8cca3e51c9b084f6e461ea46e4fb8823208
Johannes Kvamme <johanneskv96@gmail.com>
2022-01-20 10:23:02 +0100

    Remove no-longer needed comment
    Co-authored-by: Ingar Almklov <ingara@gmail.com>

--- src/index.js
+++ src/index.js
@@ -3,5 +3,4 @@ import { Elm } from './Main.elm';
 
 
 const node = document.querySelector("#app div")
-// Bug i elm-webpack-loader gjør at vi får Elm.Elm her; https://github.com/elm-community/elm-webpack-loader/issues/149
 Elm.Main.init({ node: node });
